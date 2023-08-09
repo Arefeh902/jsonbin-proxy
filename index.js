f74ac43f4d53bab555fbe93fbe1c7835244506c3
Arefeh Ahmadi <75084624+Arefeh902@users.noreply.github.com>
2023-08-09 16:45:25 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { kv } = require('@vercel/kv');
+const {kv} = require('@vercel/kv');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+const key = 'yourKey'; // define your key name here
 
-app.post('/api/data', async function(req, res) {
-    let key = 'yourKey'; // define your key name here
+app.post('/api/data', async function (req, res) {
     try {
-        await kv.append(key, JSON.stringify(req.body) + ',');
-        res.send({ status: 'SUCCESS', message: 'Data added successfully.' });
-    } catch(err) {
+        await kv.append(key, JSON.stringify(req.body) + '++');
+        res.send({status: 'SUCCESS', message: 'Data added successfully.'});
+    } catch (err) {
         console.log(err);
-        res.send({ status: 'ERROR', message: 'Failed to add data.' });
+        res.send({status: 'ERROR', message: 'Failed to add data.'});
     }
 });
 
-app.get('/api/data', async function(req, res) {
-    let key = 'yourKey'; // the same key name you used in POST request
+app.get('/api/data', async function (req, res) {
     try {
-        let dataBlobs = (await kv.get(key)).split(',');
-        let receivedData = dataBlobs.map(JSON.parse).filter(Boolean); // filter to remove any empty strings
-        res.send({ data: receivedData });
-    } catch(err) {
+        const receivedData = await kv.get(key);
+        res.send({
+            data: receivedData?.split('++').map((d) => {
+                try {
+                    return JSON.parse(d)
+                } catch (_e) {
+                    return d
+                }
+            }).filter(Boolean)
+        });
+    } catch (err) {
         console.log(err);
-        res.send({ status: 'ERROR', message: 'Failed to retrieve data.' });
+        res.send({status: 'ERROR', message: 'Failed to retrieve data.'});
     }
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('The server starts running on port 3000');
 });
 
