420d6fc60c54a683907263fdee281ee1e1b7724f
Arefeh Ahmadi <75084624+Arefeh902@users.noreply.github.com>
2023-08-09 16:22:52 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const { kv } = require('@vercel/kv');
 
-let receivedData = [];
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/data', function(req, res) {
-    receivedData.push(req.body);
-    res.send({ status: 'SUCCESS', message: 'Data added successfully' });
+app.post('/api/data', async function(req, res) {
+    let key = 'yourKey'; // define your key name here
+    try {
+        await kv.append(key, JSON.stringify(req.body) + ',');
+        res.send({ status: 'SUCCESS', message: 'Data added successfully.' });
+    } catch(err) {
+        console.log(err);
+        res.send({ status: 'ERROR', message: 'Failed to add data.' });
+    }
 });
 
-app.get('/api/data', function(req, res) {
-    res.send({ data: receivedData });
+app.get('/api/data', async function(req, res) {
+    let key = 'yourKey'; // the same key name you used in POST request
+    try {
+        let dataBlobs = (await kv.get(key)).split(',');
+        let receivedData = dataBlobs.map(JSON.parse).filter(Boolean); // filter to remove any empty strings
+        res.send({ data: receivedData });
+    } catch(err) {
+        console.log(err);
+        res.send({ status: 'ERROR', message: 'Failed to retrieve data.' });
+    }
 });
 
 app.listen(3000, function() {
