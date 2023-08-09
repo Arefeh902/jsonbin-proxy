abfa04723eafb95d75e8658aceccbb61191229e9
Arefeh Ahmadi <75084624+Arefeh902@users.noreply.github.com>
2023-08-09 17:06:24 +0330

    handle reset

--- pages/api/reset.js
+++ pages/api/reset.js
@@ -4,4 +4,5 @@ const key = 'yourKey'; // define your key name here
 
 export default async function handler(req, res) {
   kv.del(key)
+  res.status(200).json({ status: 'SUCCESS', message: 'reset successfully.' })
 }
